2bc15e338ad39b24be26a3381a60f86d2693aebf
Miller <dhanyalayaz786@gmail.com>
2022-05-09 17:11:24 +0100

    index.html

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
-// #1 - Object which contains the choices rock, paper, scissors
-const choices = ['Rock', 'Paper', 'Scissors']
+// #1 - Array which contains the choices rock, paper, scissors
+const myArray = ['Rock', 'Paper', 'Scissors']
 // #2 - Function which randomly picks one of the choices
 function computerPlay() {
-    const random = choices[Math.floor(Math.random() * choices.length)]; 
+    const random = myArray[Math.floor(Math.random() * myArray.length)]; 
     return random;
 }
-console.log(computerPlay()); 
+
+// #3 - Function to play through a round and determines winner
+let playerSelection;
+let computerSelection = computerPlay();
+
+function playRound (playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerPlay().toLowerCase();
+
+    if (playerSelection == computerSelection) {
+        return 'Its a tie!'
+    } else if (
+       (playerSelection == 'rock' && computerSelection == 'paper') || 
+       (playerSelection == 'paper' && computerSelection == 'scissors') || 
+       (playerSelection == 'scissors' && computerSelection == 'rock')
+    ) {
+        return `You lose! ${(computerSelection)} beats ${(playerSelection)}`
+    } else { 
+        return `You win! ${(playerSelection)} beats ${(computerSelection)}`
+    }
+}
+
+console.log(playRound('rock'))
 
